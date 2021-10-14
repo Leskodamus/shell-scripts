5bbc9226ada305fd3ae9947b2504620c21ebb955
Leskodamus <florian@leskovsek.dev>
2021-10-14 17:47:59 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Linux Shell Scripts 
+
+Various personal shell (bash) scripts for pretty much anything. Use them at your own risk!
---
+++ setup_links.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# copy scripts into $HOME/.local/bin
+ln -sf $(pwd)/battcheck $HOME/.local/bin/battcheck
+ln -sf $(pwd)/geoip $HOME/.local/bin/geoip
+ln -sf $(pwd)/lock $HOME/.local/bin/lock
+ln -sf $(pwd)/sleeplock $HOME/.local/bin/sleeplock
+ln -sf $(pwd)/myip $HOME/.local/bin/myip
+ln -sf $(pwd)/record $HOME/.local/bin/record
+ln -sf $(pwd)/xdisplay $HOME/.local/bin/xdisplay
+ln -sf $(pwd)/nctf $HOME/.local/bin/nctf
+ln -sf $(pwd)/toggaudio $HOME/.local/bin/toggaudio
+ln -sf $(pwd)/todo $HOME/.local/bin/todo
+ln -sf $(pwd)/casioclasspad $HOME/.local/bin/casioclasspad
