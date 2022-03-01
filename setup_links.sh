57e2df7825976d8f16219bf91d4f31be831ce32f
Leskodamus <florian@leskovsek.dev>
2022-03-01 15:11:19 +0100

    cleanup

--- setup_links.sh
+++ setup_links.sh
@@ -11,3 +11,5 @@ ln -sf $(pwd)/nctf $HOME/.local/bin/nctf
 ln -sf $(pwd)/toggaudio $HOME/.local/bin/toggaudio
 ln -sf $(pwd)/todo $HOME/.local/bin/todo
 ln -sf $(pwd)/casioclasspad $HOME/.local/bin/casioclasspad
+ln -sf $(pwd)/mdweb $HOME/.local/bin/mdweb
+ln -sf $(pwd)/cloudbackup $HOME/.local/bin/cloudbackup
