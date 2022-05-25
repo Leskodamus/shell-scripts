b723c697799f1fec08d2443851503c0d9ad712a8
Leskodamus <florian@leskovsek.dev>
2022-05-25 10:48:28 +0200

    removed xdisplay

--- setup_links.sh
+++ setup_links.sh
@@ -6,7 +6,6 @@ ln -sf $(pwd)/geoip $HOME/.local/bin/geoip
 ln -sf $(pwd)/sleeplock $HOME/.local/bin/sleeplock
 ln -sf $(pwd)/myip $HOME/.local/bin/myip
 ln -sf $(pwd)/record $HOME/.local/bin/record
-ln -sf $(pwd)/xdisplay $HOME/.local/bin/xdisplay
 ln -sf $(pwd)/nctf $HOME/.local/bin/nctf
 ln -sf $(pwd)/toggaudio $HOME/.local/bin/toggaudio
 ln -sf $(pwd)/todo $HOME/.local/bin/todo
